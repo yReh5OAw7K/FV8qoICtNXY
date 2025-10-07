d04fedffc3e404230e4685fc38b478146580d057
yReh5OAw7K <114463017+yReh5OAw7K@users.noreply.github.com>
2025-10-07 14:29:12 +0100

    优化了 网络请求 模块的性能

---
+++ contact.php
@@ -0,0 +1,26 @@
+<?php include("header.html")
+?>
+<body>
+<div class="contact">
+<div class="map">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3917.10234520729!2d78.07952957504449!3d10.955641189204217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3baa2fbbe3ae8f07%3A0xbeb5a91ebf1bc219!2sLight%20House%20Corner%20Bus%20Stop!5e0!3m2!1sen!2sin!4v1752299708133!5m2!1sen!2sin" width="1000" height="300px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+<div class="address">
+<P>
+No 23,<br> Rathinam street,<br> Light House Corner,<br> Karur-639001
+</P>
+</div>
+<div class="contact1">
+<p>
+Mobile Number:9994666135<br>
+Whatsapp Number:9442625234<br>
+Twitter Id:ayrus<br>
+Instagram Id:nidan._.ayrus<br>
+FaceBook Id:nadin_surya<br>
+Email Id:ayrus@gmail.com<br>
+</p>
+</div>
+</div>
+</body>
+<?php include("footer.html")
+?>
